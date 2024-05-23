3c16242ec09317a77fe88531bc0c28b1ffaf6652
Abraralvi2002 <abrarsiddiqui734@gmail.com>
2024-05-23 21:04:09 +0600

    Solution of quadratic equation

---
+++ Codes/Solution_of_equation.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int a, b, c;
+    double d, x1, x2;
+
+    printf("Enter the value of 'A', 'B' and 'C' of 'Ax^2 + Bx + C = 0'\n");
+    scanf("%d %d %d", &a, &b, &c);
+    d = (b * b) - (4 * a * c);
+
+    if (d > 0) {
+        x1 = (-b + sqrt(d)) / (2 * a);
+        x2 = (-b - sqrt(d)) / (2 * a);
+        printf("The roots are real and different.\n");
+        printf("X1 = %.2lf\n", x1);
+        printf("X2 = %.2lf\n", x2);
+    } else if (d == 0) {
+        x1 = -b / (2 * a);
+        printf("The root is real and the same.\n");
+        printf("X = %.2lf\n", x1);
+    } else {
+        double realPart = -b / (2.0 * a);
+        double imaginaryPart = sqrt(-d) / (2.0 * a);
+        printf("The roots are complex and different.\n");
+        printf("X1 = %.2lf + %.2lfi\n", realPart, imaginaryPart);
+        printf("X2 = %.2lf - %.2lfi\n", realPart, imaginaryPart);
+    }
+
+    return 0;
+}

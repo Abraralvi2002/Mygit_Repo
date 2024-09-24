2aede756c11a8e2316ddc116c077e61e71fc965c
Md. Abrar Siddiqui <abrarsiddiqui734@gmail.com>
2024-09-24 19:47:51 +0600

    Escape secquence

---
+++ Java/My_Project/Escape_secuence.java
@@ -0,0 +1,6 @@
+public class Escape_secuence {
+    public static void main(String[] args){
+        System.out.println("1 \t 2\n3 \t 4");
+        System.out.println("\"Java Programming\" ");
+    }
+}

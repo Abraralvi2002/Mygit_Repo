f90eb8a5f49ddcaae5e5b5758ae8eebbf51fab43
Md. Abrar Siddiqui <abrarsiddiqui734@gmail.com>
2024-09-24 20:43:42 +0600

    Update

---
+++ Java/My_Project/src/Datatypes.java
@@ -0,0 +1,18 @@
+public class Datatypes {
+    public static void main(String[] args){
+
+       boolean b;
+       char c;
+       short s;
+       int i;
+       float f;
+       double d;
+
+       b = true;
+       System.out.println("B is: "+b);
+       c = 'a';
+       System.out.println("c = "+c);
+       
+
+    }
+}

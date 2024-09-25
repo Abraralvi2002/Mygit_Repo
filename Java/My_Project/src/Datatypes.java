5fb6e0dc8e62b5c01dfd878d694cf9289ae83f7e
Md. Abrar Siddiqui <abrarsiddiqui734@gmail.com>
2024-09-25 21:53:05 +0600

    Product and datatypes in java

--- Java/My_Project/src/Datatypes.java
+++ Java/My_Project/src/Datatypes.java
@@ -12,6 +12,14 @@ public class Datatypes {
        System.out.println("B is: "+b);
        c = 'a';
        System.out.println("c = "+c);
+       s = -5426;
+       System.out.println("Short is: "+s);
+       i = 123456;
+       System.out.println("Int is: "+i);
+       f = 30;
+       System.out.println("Float is: "+f);
+       d = 50.122;
+       System.out.println("Double is: "+d);
        
 
     }

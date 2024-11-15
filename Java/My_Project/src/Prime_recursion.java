758fc2ea75a6c98929125f2e04471148e23972c2
Md. Abrar Siddiqui <abrarsiddiqui734@gmail.com>
2024-11-15 20:14:01 +0600

    solve of questuion

--- Java/My_Project/src/Prime_recursion.java
+++ Java/My_Project/src/Prime_recursion.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Prime_recursion {
-    public boolean Prime(int num){
-        if()
+    public static boolean isPrime(int num, int i) {
+        if (num <= 2) return num == 2;
+        if (num % i == 0) return false;
+        if (i > Math.sqrt(num)) return true;
+        return isPrime(num, i + 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner ipt = new Scanner(System.in);
+        System.out.println("Enter the number to check: ");
+        int num = ipt.nextInt();
+        if (isPrime(num, 2)) {
+            System.out.println(num + " is a prime number.");
+        } else {
+            System.out.println(num + " is not a prime number.");
+        }
     }
 }

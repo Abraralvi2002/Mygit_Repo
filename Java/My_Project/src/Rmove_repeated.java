bc382cfae3c93761a4dfd022350af69d8575acff
Md. Abrar Siddiqui <abrarsiddiqui734@gmail.com>
2024-12-01 02:44:00 +0600

    Problem 3

---
+++ Java/My_Project/src/Rmove_repeated.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Rmove_repeated {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the string: ");
+        String str = sc.nextLine();
+        String result = rmvr(str);
+        System.out.println("String after removing repeated char: " + result);
+    }
+
+    public static String rmvr(String str) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            if (result.indexOf(String.valueOf(str.charAt(i))) == -1) {
+                result.append(str.charAt(i));
+            }
+        }
+        return result.toString();
+    }
+}

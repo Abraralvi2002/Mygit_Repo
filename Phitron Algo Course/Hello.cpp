bef69a13020029cc2a2070c121ea475859c81d26
Md. Abrar Siddiqui <abrarsiddiqui734@gmail.com>
2025-08-21 21:14:53 +0600

    testing my setup

---
+++ Phitron Algo Course/Hello.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    cout<<"Hello How are you???"<<endl;
+    cout<<"Now give some input ..."<<endl;
+    int a,b;
+    cin>>a>>b;
+    cout<<"The sum is : "<<a+b<<endl;
+    return 0;
+}
+// just testing the vs code setup

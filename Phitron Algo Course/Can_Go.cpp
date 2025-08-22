3fb613d1810700057dc20d4a0693c0157588b8dd
Md. Abrar Siddiqui <abrarsiddiqui734@gmail.com>
2025-08-22 20:32:09 +0600

    solved another one

---
+++ Phitron Algo Course/Can_Go.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+char grid[1005][1005];
+bool vis[1005][1005];
+vector<pair<int,int>> shift = {{-1,0},{1,0},{0,-1},{0,1}};
+int n,m,si,sj;
+bool flag = false;
+
+bool vaild(int i, int j){
+    if(i<0 || i>=n || j<0 || j>=m ){
+        return false;
+    }
+    return true;
+}
+
+void dfs(int si, int sj){
+
+    vis[si][sj] = true;
+    for(int i = 0;i<4;i++){
+        int ci = si + shift[i].first;
+        int cj = sj + shift[i].second;
+        if(vaild(ci,cj) == true && !vis[ci][cj] && grid[ci][cj] != '#'){
+            if(grid[ci][cj] == 'B'){
+                flag = true;
+                return;
+            }
+            dfs(ci,cj);
+        }
+    }
+}
+
+int main(){
+    
+    cin>>n>>m;
+
+    for(int i = 0;i<n;i++){
+        for(int j = 0;j<m;j++){
+            cin>>grid[i][j];
+            if(grid[i][j] == 'A'){
+                si = i;
+                sj = j;
+            }
+        }
+    }
+    memset(vis,false,sizeof(vis));
+
+    dfs(si,sj);
+    if(!flag){
+        cout<<"NO";
+    }else{
+        cout<<"YES";
+    }
+    return 0;
+}
